cd6303ed7efaaf912b3f06b54d954fdc7c26aafb
t.chambard <t.chambard@bee-buzziness.com>
2024-07-04 22:54:45 +0200

    chore: improve abstract solana client implementation

--- packages/programs/solidr-program/migrations/initGlobal.ts
+++ packages/programs/solidr-program/migrations/initGlobal.ts
@@ -8,25 +8,25 @@ dotenv.config();
 
 const walletSecretKey = process.env.WALLET_SECRET_KEY;
 if (!walletSecretKey) {
-  throw new Error('Missing WALLET_SECRET_KEY in .env');
+    throw new Error('Missing WALLET_SECRET_KEY in .env');
 }
 const anchorProviderUrl = process.env.ANCHOR_PROVIDER_URL;
 if (!anchorProviderUrl) {
-  throw new Error('Missing ANCHOR_PROVIDER_URL in .env');
+    throw new Error('Missing ANCHOR_PROVIDER_URL in .env');
 }
 const secretKey = Uint8Array.from(JSON.parse(walletSecretKey));
 const walletKeypair = Keypair.fromSecretKey(secretKey);
 
 const initGlobal = async () => {
-  const connection = new Connection(anchorProviderUrl, 'confirmed');
-  const wallet = new Wallet(walletKeypair);
-  const provider = new AnchorProvider(connection, wallet, AnchorProvider.defaultOptions());
+    const connection = new Connection(anchorProviderUrl, 'confirmed');
+    const wallet = new Wallet(walletKeypair);
+    const provider = new AnchorProvider(connection, wallet, AnchorProvider.defaultOptions());
 
-  const program = new Program<Solidr>(idl as Solidr, provider);
-  const solidrClient = new SolidrClient(program, { skipPreflight: false });
+    const program = new Program<Solidr>(idl as Solidr, provider);
+    const solidrClient = new SolidrClient(program, { skipPreflight: false });
 
-  const { tx } = await solidrClient.initGlobal(wallet);
-  console.log('Transaction successful, global account created with TX:', tx);
+    const { tx } = await solidrClient.initGlobal(wallet);
+    console.log('Transaction successful, global account created with TX:', tx);
 };
 
 initGlobal().catch(console.error);
