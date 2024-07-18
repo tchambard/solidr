236edb0f3a697578476e86a7f41ce1b09e39c411
Nicolas Coquelet <nicolascoquelet@gmail.com>
2024-07-18 19:44:59 +0200

    re-add add session button

--- packages/fronts/solidr-front-dapp/src/content/solidr/components/list/SessionListContainer.tsx
+++ packages/fronts/solidr-front-dapp/src/content/solidr/components/list/SessionListContainer.tsx
@@ -4,6 +4,7 @@ import { Helmet } from 'react-helmet-async';
 import PageTitleWrapper from '@/components/PageTitleWrapper';
 import SessionsListTable from './SessionListTable';
 import SessionNavigation from '@/content/solidr/components/navigation/SessionNavigation';
+import SessionCreateButton from '@/content/solidr/components/list/SessionCreateButton';
 
 export default () => {
     return (
@@ -12,7 +13,14 @@ export default () => {
                 <title>Solidr sessions</title>
             </Helmet>
             <PageTitleWrapper>
-                <SessionNavigation />
+                <Grid container direction="row" justifyContent="space-between" alignItems="center">
+                    <Grid>
+                        <SessionNavigation />
+                    </Grid>
+                    <Grid>
+                        <SessionCreateButton />
+                    </Grid>
+                </Grid>
             </PageTitleWrapper>
             <Container maxWidth={'xl'}>
                 <Grid container direction={'row'} justifyContent={'center'} alignItems={'stretch'} spacing={3}>
