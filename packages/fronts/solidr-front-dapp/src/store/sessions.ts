d98fcc6b31c873600b2fc5853f01652989d76893
Nicolas Coquelet <nicolascoquelet@gmail.com>
2024-07-18 11:47:28 +0200

    Add prettier config + reformat

--- packages/fronts/solidr-front-dapp/src/store/sessions.ts
+++ packages/fronts/solidr-front-dapp/src/store/sessions.ts
@@ -1,43 +1,36 @@
-import { atom } from "recoil";
-import {
-  Expense,
-  MemberBalance,
-  MemberTransfer,
-  Refund,
-  Session,
-  SessionMember,
-} from "@solidr";
+import { atom } from 'recoil';
+import { Expense, MemberBalance, MemberTransfer, Refund, Session, SessionMember } from '@solidr';
 
 type SessionListState = { items: Session[]; loaded: boolean };
 
 export const sessionListState = atom<SessionListState>({
-  key: "sessionListState",
-  default: { items: [], loaded: false },
+    key: 'sessionListState',
+    default: { items: [], loaded: false },
 });
 
 export type SessionCurrentState = {
-  session: Session | undefined;
-  members: { [publicKey: string]: SessionMember };
-  expenses: Expense[];
-  refunds: Refund[];
-  balances: { [publicKey: string]: MemberBalance };
-  transfers: MemberTransfer[];
-  myTotalCost: number;
-  totalExpenses: number;
-  isAdmin: boolean;
+    session: Session | undefined;
+    members: { [publicKey: string]: SessionMember };
+    expenses: Expense[];
+    refunds: Refund[];
+    balances: { [publicKey: string]: MemberBalance };
+    transfers: MemberTransfer[];
+    myTotalCost: number;
+    totalExpenses: number;
+    isAdmin: boolean;
 };
 
 export const sessionCurrentState = atom<SessionCurrentState>({
-  key: "sessionCurrentState",
-  default: {
-    session: undefined,
-    members: {},
-    expenses: [],
-    refunds: [],
-    balances: {},
-    transfers: [],
-    myTotalCost: 0,
-    totalExpenses: 0,
-    isAdmin: false,
-  },
+    key: 'sessionCurrentState',
+    default: {
+        session: undefined,
+        members: {},
+        expenses: [],
+        refunds: [],
+        balances: {},
+        transfers: [],
+        myTotalCost: 0,
+        totalExpenses: 0,
+        isAdmin: false,
+    },
 });
