48b87aa77c3a39fe7cfa52d8d1facc7fff79c7c1
Nicolas Coquelet <nicolascoquelet@gmail.com>
2024-07-18 11:22:54 +0200

    Add prettier config + reformat

--- packages/fronts/solidr-front-dapp/src/content/solidr/components/detail/SessionBalance.tsx
+++ packages/fronts/solidr-front-dapp/src/content/solidr/components/detail/SessionBalance.tsx
@@ -1,53 +1,51 @@
-import { useRecoilValue } from "recoil";
-import Grid from "@mui/material/Grid";
-import Typography from "@mui/material/Typography";
-import { useTheme } from "@mui/material/styles";
-import Divider from "@mui/material/Divider";
+import { useRecoilValue } from 'recoil';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import { useTheme } from '@mui/material/styles';
+import Divider from '@mui/material/Divider';
 
-import PageTitleWrapper from "@/components/PageTitleWrapper";
-import { sessionCurrentState } from "@/store/sessions";
-import * as _ from "lodash";
-import ListItem from "@mui/material/ListItem";
-import Tooltip from "@mui/material/Tooltip";
-import ListItemAvatar from "@mui/material/ListItemAvatar";
-import AddressAvatar from "@/components/AddressAvatar";
-import ListItemText from "@mui/material/ListItemText";
-import List from "@mui/material/List";
+import PageTitleWrapper from '@/components/PageTitleWrapper';
+import { sessionCurrentState } from '@/store/sessions';
+import * as _ from 'lodash';
+import ListItem from '@mui/material/ListItem';
+import Tooltip from '@mui/material/Tooltip';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import AddressAvatar from '@/components/AddressAvatar';
+import ListItemText from '@mui/material/ListItemText';
+import List from '@mui/material/List';
 
 export default () => {
-  const theme = useTheme();
+    const theme = useTheme();
 
-  const sessionCurrent = useRecoilValue(sessionCurrentState);
+    const sessionCurrent = useRecoilValue(sessionCurrentState);
 
-  return (
-    <>
-      <PageTitleWrapper>
-        <Grid container justifyContent={"space-between"} alignItems={"center"}>
-          <Grid item>
-            <Typography variant={"h3"} component={"h3"} gutterBottom>
-              Balances
-            </Typography>
-          </Grid>
-        </Grid>
-      </PageTitleWrapper>
+    return (
+        <>
+            <PageTitleWrapper>
+                <Grid container justifyContent={'space-between'} alignItems={'center'}>
+                    <Grid item>
+                        <Typography variant={'h3'} component={'h3'} gutterBottom>
+                            Balances
+                        </Typography>
+                    </Grid>
+                </Grid>
+            </PageTitleWrapper>
 
-      <Divider variant={"middle"} />
+            <Divider variant={'middle'} />
 
-      <List sx={{ width: "100%", bgcolor: "background.paper" }}>
-        {_.map(sessionCurrent.balances, (owner, address) => (
-          <ListItem key={`voter_${address}`}>
-            <Tooltip title={address}>
-              <ListItemAvatar>
-                <AddressAvatar address={address} />
-              </ListItemAvatar>
-            </Tooltip>
-            <ListItemText
-              primary={sessionCurrent.members[owner.owner.toString()].name}
-            />
-            <ListItemText primary={owner.balance} />
-          </ListItem>
-        ))}
-      </List>
-    </>
-  );
+            <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
+                {_.map(sessionCurrent.balances, (owner, address) => (
+                    <ListItem key={`voter_${address}`}>
+                        <Tooltip title={address}>
+                            <ListItemAvatar>
+                                <AddressAvatar address={address} />
+                            </ListItemAvatar>
+                        </Tooltip>
+                        <ListItemText primary={sessionCurrent.members[owner.owner.toString()].name} />
+                        <ListItemText primary={owner.balance} />
+                    </ListItem>
+                ))}
+            </List>
+        </>
+    );
 };
--- packages/fronts/solidr-front-dapp/src/content/solidr/components/detail/SessionContainer.tsx
+++ packages/fronts/solidr-front-dapp/src/content/solidr/components/detail/SessionContainer.tsx
@@ -1,217 +1,182 @@
-import { Suspense, useEffect } from "react";
-import { Helmet } from "react-helmet-async";
-import { Container, Grid, Paper } from "@mui/material";
+import { Suspense, useEffect } from 'react';
+import { Helmet } from 'react-helmet-async';
+import { Container, Grid, Paper } from '@mui/material';
 
-import PageTitleWrapper from "@/components/PageTitleWrapper";
+import PageTitleWrapper from '@/components/PageTitleWrapper';
 
-import SessionHeader from "./SessionHeader";
-import { useParams } from "react-router";
+import SessionHeader from './SessionHeader';
+import { useParams } from 'react-router';
 
-import SessionMemberList from "./SessionMemberList";
-import SessionExpenseList from "./SessionExpenseList";
-import styled from "@mui/styles/styled";
-import { useRecoilState, useRecoilValue } from "recoil";
-import { SessionCurrentState, sessionCurrentState } from "@/store/sessions";
-import BN from "bn.js";
-import AppLoading from "@/components/loading/AppLoading";
-import { solidrClientState } from "@/store/wallet";
-import { useAnchorWallet } from "@solana/wallet-adapter-react";
-import { Wallet } from "@coral-xyz/anchor";
-import { SessionMember, SessionStatus } from "@solidr";
-import SessionBalance from "@/content/solidr/components/detail/SessionBalance";
-import SessionTransfers from "@/content/solidr/components/detail/SessionTransfers";
+import SessionMemberList from './SessionMemberList';
+import SessionExpenseList from './SessionExpenseList';
+import styled from '@mui/styles/styled';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { SessionCurrentState, sessionCurrentState } from '@/store/sessions';
+import BN from 'bn.js';
+import AppLoading from '@/components/loading/AppLoading';
+import { solidrClientState } from '@/store/wallet';
+import { useAnchorWallet } from '@solana/wallet-adapter-react';
+import { Wallet } from '@coral-xyz/anchor';
+import { SessionMember, SessionStatus } from '@solidr';
+import SessionBalance from '@/content/solidr/components/detail/SessionBalance';
+import SessionTransfers from '@/content/solidr/components/detail/SessionTransfers';
 
 const Item = styled(Paper)(({ theme }) => ({
-  // color: theme.palette.text.secondary,
+    // color: theme.palette.text.secondary,
 }));
 
 export default () => {
-  const { sessionId } = useParams();
-  const [sessionCurrent, setSessionCurrent] =
-    useRecoilState(sessionCurrentState);
-  const solidrClient = useRecoilValue(solidrClientState);
-  const anchorWallet = useAnchorWallet() as Wallet;
+    const { sessionId } = useParams();
+    const [sessionCurrent, setSessionCurrent] = useRecoilState(sessionCurrentState);
+    const solidrClient = useRecoilValue(solidrClientState);
+    const anchorWallet = useAnchorWallet() as Wallet;
 
-  useEffect(() => {
-    if (solidrClient == null || sessionId == null) return;
+    useEffect(() => {
+        if (solidrClient == null || sessionId == null) return;
 
-    const listeners: number[] = [];
+        const listeners: number[] = [];
 
-    const reloadSessionBalance = (
-      sessionCurrent: SessionCurrentState,
-      anchorWallet: Wallet,
-    ) => {
-      if (sessionCurrent.session) {
-        solidrClient
-          .computeBalance(
-            Object.values(sessionCurrent.members),
-            sessionCurrent.expenses,
-            sessionCurrent.refunds,
-          )
-          .then((data) => {
-            const { totalExpenses, members, transfers } = data;
-            setSessionCurrent({
-              ...sessionCurrent,
-              balances: members,
-              transfers,
-              myTotalCost: members[anchorWallet.publicKey.toString()].totalCost,
-              totalExpenses,
-            });
-          });
-      }
-    };
-
-    if (
-      !sessionCurrent.session ||
-      sessionCurrent.session.sessionId.toString() !== sessionId
-    ) {
-      const sid = new BN(sessionId);
-      const sessionAccountAddress = solidrClient.findSessionAccountAddress(sid);
-      Promise.all([
-        solidrClient.getSession(sessionAccountAddress),
-        // TODO: manage pagination for members and expenses
-        solidrClient.listSessionMembers(sid),
-        solidrClient.listSessionExpenses(sid),
-      ]).then(([session, members, expenses]) => {
-        const newSessionState: SessionCurrentState = {
-          session,
-          members: members.reduce(
-            (acc, member) => {
-              acc[member.addr.toString()] = member;
-              return acc;
-            },
-            {} as { [pubkey: string]: SessionMember },
-          ),
-          expenses,
-          refunds: [],
-          balances: {},
-          transfers: [],
-          myTotalCost: 0,
-          totalExpenses: 0,
-          isAdmin:
-            anchorWallet?.publicKey.toString() === session.admin.toString(),
+        const reloadSessionBalance = (sessionCurrent: SessionCurrentState, anchorWallet: Wallet) => {
+            if (sessionCurrent.session) {
+                solidrClient.computeBalance(Object.values(sessionCurrent.members), sessionCurrent.expenses, sessionCurrent.refunds).then((data) => {
+                    const { totalExpenses, members, transfers } = data;
+                    setSessionCurrent({
+                        ...sessionCurrent,
+                        balances: members,
+                        transfers,
+                        myTotalCost: members[anchorWallet.publicKey.toString()].totalCost,
+                        totalExpenses,
+                    });
+                });
+            }
         };
-        setSessionCurrent(newSessionState);
-        reloadSessionBalance(newSessionState, anchorWallet);
-      });
-    } else {
-      const sessionStatusChangesListener = solidrClient.addEventListener(
-        "sessionClosed",
-        (event) => {
-          if (!sessionCurrent.session) {
-            return;
-          }
-          setSessionCurrent({
-            ...sessionCurrent,
-            session: {
-              ...sessionCurrent.session,
-              status: SessionStatus.Closed,
-            },
-          });
-          reloadSessionBalance(sessionCurrent, anchorWallet);
-        },
-      );
-      sessionStatusChangesListener &&
-        listeners.push(sessionStatusChangesListener);
 
-      const memberRegistrationListener = solidrClient.addEventListener(
-        "memberAdded",
-        (event) => {
-          solidrClient
-            .listSessionMembers(sessionCurrent.session?.sessionId)
-            .then((members) => {
-              setSessionCurrent({
-                ...sessionCurrent,
-                members: members.reduce(
-                  (acc, member) => {
-                    acc[member.addr.toString()] = member;
-                    return acc;
-                  },
-                  {} as { [pubkey: string]: SessionMember },
-                ),
-              });
-              reloadSessionBalance(sessionCurrent, anchorWallet);
+        if (!sessionCurrent.session || sessionCurrent.session.sessionId.toString() !== sessionId) {
+            const sid = new BN(sessionId);
+            const sessionAccountAddress = solidrClient.findSessionAccountAddress(sid);
+            Promise.all([
+                solidrClient.getSession(sessionAccountAddress),
+                // TODO: manage pagination for members and expenses
+                solidrClient.listSessionMembers(sid),
+                solidrClient.listSessionExpenses(sid),
+            ]).then(([session, members, expenses]) => {
+                const newSessionState: SessionCurrentState = {
+                    session,
+                    members: members.reduce(
+                        (acc, member) => {
+                            acc[member.addr.toString()] = member;
+                            return acc;
+                        },
+                        {} as { [pubkey: string]: SessionMember },
+                    ),
+                    expenses,
+                    refunds: [],
+                    balances: {},
+                    transfers: [],
+                    myTotalCost: 0,
+                    totalExpenses: 0,
+                    isAdmin: anchorWallet?.publicKey.toString() === session.admin.toString(),
+                };
+                setSessionCurrent(newSessionState);
+                reloadSessionBalance(newSessionState, anchorWallet);
             });
-        },
-      );
-      memberRegistrationListener && listeners.push(memberRegistrationListener);
+        } else {
+            const sessionStatusChangesListener = solidrClient.addEventListener('sessionClosed', (event) => {
+                if (!sessionCurrent.session) {
+                    return;
+                }
+                setSessionCurrent({
+                    ...sessionCurrent,
+                    session: {
+                        ...sessionCurrent.session,
+                        status: SessionStatus.Closed,
+                    },
+                });
+                reloadSessionBalance(sessionCurrent, anchorWallet);
+            });
+            sessionStatusChangesListener && listeners.push(sessionStatusChangesListener);
 
-      const expensesRegistrationListener = solidrClient.addEventListener(
-        "expenseAdded",
-        (event) => {
-          solidrClient
-            .listSessionExpenses(sessionCurrent.session?.sessionId)
-            .then((expenses) => {
-              if (!sessionCurrent.session) {
-                return;
-              }
-              setSessionCurrent({
-                ...sessionCurrent,
-                expenses,
-                session: {
-                  ...sessionCurrent.session,
-                  expensesCount: sessionCurrent.session.expensesCount + 1,
-                },
-              });
-              reloadSessionBalance(sessionCurrent, anchorWallet);
+            const memberRegistrationListener = solidrClient.addEventListener('memberAdded', (event) => {
+                solidrClient.listSessionMembers(sessionCurrent.session?.sessionId).then((members) => {
+                    setSessionCurrent({
+                        ...sessionCurrent,
+                        members: members.reduce(
+                            (acc, member) => {
+                                acc[member.addr.toString()] = member;
+                                return acc;
+                            },
+                            {} as { [pubkey: string]: SessionMember },
+                        ),
+                    });
+                    reloadSessionBalance(sessionCurrent, anchorWallet);
+                });
             });
-        },
-      );
-      expensesRegistrationListener &&
-        listeners.push(expensesRegistrationListener);
+            memberRegistrationListener && listeners.push(memberRegistrationListener);
+
+            const expensesRegistrationListener = solidrClient.addEventListener('expenseAdded', (event) => {
+                solidrClient.listSessionExpenses(sessionCurrent.session?.sessionId).then((expenses) => {
+                    if (!sessionCurrent.session) {
+                        return;
+                    }
+                    setSessionCurrent({
+                        ...sessionCurrent,
+                        expenses,
+                        session: {
+                            ...sessionCurrent.session,
+                            expensesCount: sessionCurrent.session.expensesCount + 1,
+                        },
+                    });
+                    reloadSessionBalance(sessionCurrent, anchorWallet);
+                });
+            });
+            expensesRegistrationListener && listeners.push(expensesRegistrationListener);
+        }
+
+        return () => {
+            listeners.forEach((listener) => {
+                solidrClient.program.removeEventListener(listener);
+            });
+        };
+    }, [sessionCurrent.session?.sessionId, sessionCurrent.session?.status]);
+
+    if (!sessionCurrent.session) {
+        return <Suspense fallback={<AppLoading />} />;
     }
 
-    return () => {
-      listeners.forEach((listener) => {
-        solidrClient.program.removeEventListener(listener);
-      });
-    };
-  }, [sessionCurrent.session?.sessionId, sessionCurrent.session?.status]);
-
-  if (!sessionCurrent.session) {
-    return <Suspense fallback={<AppLoading />} />;
-  }
-
-  return (
-    <>
-      <Helmet>
-        <title>
-          {sessionCurrent.session.name} - {sessionCurrent.session.description}
-        </title>
-      </Helmet>
-      <PageTitleWrapper>
-        <SessionHeader />
-      </PageTitleWrapper>
-      <Container maxWidth={"xl"}>
-        <Grid
-          container
-          direction={"row"}
-          justifyContent={"center"}
-          alignItems={"stretch"}
-          spacing={3}
-        >
-          <Grid item xs={12} md={6}>
-            <Item>
-              <SessionMemberList />
-            </Item>
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <Item>
-              <SessionExpenseList />
-            </Item>
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <Item>
-              <SessionBalance />
-            </Item>
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <Item>
-              <SessionTransfers />
-            </Item>
-          </Grid>
-        </Grid>
-      </Container>
-    </>
-  );
+    return (
+        <>
+            <Helmet>
+                <title>
+                    {sessionCurrent.session.name} - {sessionCurrent.session.description}
+                </title>
+            </Helmet>
+            <PageTitleWrapper>
+                <SessionHeader />
+            </PageTitleWrapper>
+            <Container maxWidth={'xl'}>
+                <Grid container direction={'row'} justifyContent={'center'} alignItems={'stretch'} spacing={3}>
+                    <Grid item xs={12} md={6}>
+                        <Item>
+                            <SessionMemberList />
+                        </Item>
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                        <Item>
+                            <SessionExpenseList />
+                        </Item>
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                        <Item>
+                            <SessionBalance />
+                        </Item>
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                        <Item>
+                            <SessionTransfers />
+                        </Item>
+                    </Grid>
+                </Grid>
+            </Container>
+        </>
+    );
 };
