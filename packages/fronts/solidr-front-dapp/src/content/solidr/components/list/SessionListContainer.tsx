5ca2cf2bc1ea264121358e6b705aa43a54f9d13e
Nicolas Coquelet <nicolascoquelet@gmail.com>
2024-07-18 16:33:57 +0200

    add simple breadcrumb to return to the sessions list

--- packages/fronts/solidr-front-dapp/src/content/solidr/components/list/SessionListContainer.tsx
+++ packages/fronts/solidr-front-dapp/src/content/solidr/components/list/SessionListContainer.tsx
@@ -2,8 +2,8 @@ import { Card, Container, Grid } from '@mui/material';
 import { Helmet } from 'react-helmet-async';
 
 import PageTitleWrapper from '@/components/PageTitleWrapper';
-import SessionsListHeader from './SessionListHeader';
 import SessionsListTable from './SessionListTable';
+import SessionNavigation from '@/content/solidr/components/navigation/SessionNavigation';
 
 export default () => {
     return (
@@ -12,16 +12,10 @@ export default () => {
                 <title>Solidr sessions</title>
             </Helmet>
             <PageTitleWrapper>
-                <SessionsListHeader />
+                <SessionNavigation />
             </PageTitleWrapper>
             <Container maxWidth={'xl'}>
-                <Grid
-                    container
-                    direction={'row'}
-                    justifyContent={'center'}
-                    alignItems={'stretch'}
-                    spacing={3}
-                >
+                <Grid container direction={'row'} justifyContent={'center'} alignItems={'stretch'} spacing={3}>
                     <Grid item xs={12}>
                         <Card>
                             <SessionsListTable />
