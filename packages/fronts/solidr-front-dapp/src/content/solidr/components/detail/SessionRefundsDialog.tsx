55ddcb3dadc5b2674b6647aeb8663fbe67f7f6b5
Nicolas Coquelet <nicolascoquelet@gmail.com>
2024-07-19 21:51:18 +0200

    wip refund dialog

--- packages/fronts/solidr-front-dapp/src/content/solidr/components/detail/SessionRefundsDialog.tsx
+++ packages/fronts/solidr-front-dapp/src/content/solidr/components/detail/SessionRefundsDialog.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle, Stack } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, FormControl, InputAdornment, InputLabel, OutlinedInput, Stack } from '@mui/material';
 import { useAnchorWallet } from '@solana/wallet-adapter-react';
 import { useRecoilValue } from 'recoil';
 import { solidrClientState, txState } from '@/store/wallet';
@@ -9,6 +9,7 @@ import { IDialogProps } from '@/content/solidr/components/list/SessionCreateDial
 import { FormContainer } from 'react-hook-form-mui';
 import { LoadingButton } from '@mui/lab';
 import SendIcon from '@mui/icons-material/Send';
+import { MemberTransfer } from '@solidr';
 
 interface IRegisterRefundParams {
     from: string;
@@ -27,6 +28,16 @@ export default ({ dialogVisible, setDialogVisible }: IDialogProps) => {
 
     if (!anchorWallet || !solidrClient || !sessionCurrent) return <></>;
 
+    const [transfers, setTransfers] = useState<Array<MemberTransfer>>([]);
+
+    useEffect(() => {
+        if (!sessionCurrent) {
+            return;
+        }
+
+        setTransfers(sessionCurrent.transfers.filter((transfer) => transfer.from.toString() == anchorWallet.publicKey.toString()));
+    }, [sessionCurrent.transfers]);
+
     return (
         <Dialog disableEscapeKeyDown maxWidth={'sm'} aria-labelledby={'register-expense-title'} open={dialogVisible}>
             <DialogTitle id={'register-expense-title'}>{'Refund my friends'}</DialogTitle>
@@ -41,6 +52,22 @@ export default ({ dialogVisible, setDialogVisible }: IDialogProps) => {
                     }}
                 >
                     <Stack direction={'column'}>
+                        {transfers.map((transfer) => (
+                            <>
+                                <FormControl fullWidth sx={{ m: 1 }}>
+                                    <InputLabel htmlFor={`to_${transfer.to.toString()}`}>{`to ${sessionCurrent.members[transfer.to.toString()].name}`}</InputLabel>
+                                    <OutlinedInput
+                                        id={`to_${transfer.to.toString()}`}
+                                        startAdornment={<InputAdornment position="start">€</InputAdornment>}
+                                        label={`to ${sessionCurrent.members[transfer.to.toString()].name}`}
+                                        defaultValue={transfer.amount}
+                                        type={'number'}
+                                    />
+                                </FormControl>
+                                <br />
+                            </>
+                        ))}
+
                         <LoadingButton loading={tx.pending} loadingPosition={'end'} variant={'contained'} color={'primary'} endIcon={<SendIcon />} type={'submit'}>
                             Submit
                         </LoadingButton>
