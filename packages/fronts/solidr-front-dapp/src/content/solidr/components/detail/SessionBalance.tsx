866249df0683a51f1bf844a241e10964afaf545b
Nicolas Coquelet <nicolascoquelet@gmail.com>
2024-07-20 22:01:41 +0200

    convert donut to bar chart

--- packages/fronts/solidr-front-dapp/src/content/solidr/components/detail/SessionBalance.tsx
+++ packages/fronts/solidr-front-dapp/src/content/solidr/components/detail/SessionBalance.tsx
@@ -1,11 +1,10 @@
 import { useRecoilValue } from 'recoil';
 import { sessionCurrentState } from '@/store/sessions';
-import { Doughnut } from 'react-chartjs-2';
-import { ArcElement, Chart as ChartJS, Legend, Tooltip as ChartTooltip } from 'chart.js/auto';
+import { Bar } from 'react-chartjs-2';
+import { BarElement, CategoryScale, Chart as ChartJS, LinearScale, Tooltip as ChartTooltip } from 'chart.js/auto';
 import { hexToRgba, stringToColor } from '@/lib/colors';
 
-ChartJS.register(ArcElement, ChartTooltip, Legend);
-
+ChartJS.register(BarElement, CategoryScale, LinearScale, ChartTooltip);
 export default () => {
     const sessionCurrent = useRecoilValue(sessionCurrentState);
 
@@ -20,6 +19,28 @@ export default () => {
         }
     });
 
+    const customLabelsPlugin = {
+        id: 'customLabels',
+        afterDatasetsDraw(chart, args, pluginOptions) {
+            const { ctx, data, scales } = chart;
+
+            ctx.save();
+            ctx.font = `${pluginOptions.font.weight} ${pluginOptions.font.size}px sans-serif`;
+            ctx.textAlign = 'center';
+
+            data.datasets[0].data.forEach((value, index) => {
+                const x = scales.x.getPixelForValue(index);
+                const y = scales.y.getPixelForValue(value);
+
+                ctx.fillStyle = value >= 0 ? '#71e36f' : '#e36f6f';
+                const yPos = value >= 0 ? y - 10 : y + 20;
+                ctx.fillText(`${value.toFixed(2)}€`, x, yPos);
+            });
+
+            ctx.restore();
+        },
+    };
+
     const data = {
         labels: sortedBalances.map((balance) => sessionCurrent.members[balance.owner.toString()].name),
         datasets: [
@@ -27,39 +48,57 @@ export default () => {
                 data: sortedBalances.map((balance) => balance.balance),
                 backgroundColor: sortedBalances.map((balance) => stringToColor(balance.owner.toString())),
                 hoverBackgroundColor: sortedBalances.map((balance) => hexToRgba(stringToColor(balance.owner.toString()), 0.5).toString()),
+                barPercentage: 0.9,
+                borderWidth: 0,
             },
         ],
     };
     const options = {
         plugins: {
             title: {
-                display: true,
-                text: 'Costs balance',
+                display: false,
+                text: 'balance of costs',
                 position: 'bottom' as const,
             },
             legend: {
                 display: false,
-                position: 'left' as const,
-                labels: {
-                    generateLabels: (chart) => {
-                        const data = chart.data;
-                        if (data.labels && data.datasets.length) {
-                            return data.labels.map((label, index) => ({
-                                text: `${label} ${data.datasets[0].data[index].toFixed(2)}€`,
-                                fillStyle: data.datasets[0].backgroundColor[index],
-                            }));
-                        }
-                        return [];
-                    },
+            },
+            customLabels: {
+                font: {
+                    size: 12,
+                    weight: 'bold',
                 },
             },
         },
-        cutout: '70%',
+        scales: {
+            x: {
+                display: false, // Hide x-axis completely
+            },
+            y: {
+                display: true, // Hide y-axis labels and ticks
+                beginAtZero: true,
+                grid: {
+                    drawBorder: false,
+                    color: (context) => {
+                        if (context.tick.value === 0) {
+                            return 'rgba(0, 0, 0, 0.1)'; // Color of the zero line
+                        }
+                        return 'rgba(0, 0, 0, 0)'; // Transparent color for other grid lines
+                    },
+                },
+                ticks: {
+                    display: false,
+                },
+                border: {
+                    display: false,
+                },
+            },
+        },
         layout: {
             padding: {
-                top: 20,
+                top: 40,
                 right: 50,
-                bottom: 20,
+                bottom: 40,
                 left: 0,
             },
         },
@@ -67,5 +106,5 @@ export default () => {
         responsive: true, // Set this to true to make the chart responsive
     };
 
-    return <Doughnut data={data} options={options} />;
+    return <Bar data={data} options={options} plugins={[customLabelsPlugin]} />;
 };
